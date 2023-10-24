c89eb2a64f685ade2921e9c7199d166a12b35709
Prakash K <prakash.change@gmail.com>
2023-10-24 08:40:49 +0530

    render friendslist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import './index.css'
+import "./index.css";
 
-export default App(){
-return
- <div>
- <FriendsList/>
-</div>
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <FriendsList />
+      <AddFriendForm />
+      <SplitBillForm />
+    </div>
+  );
 }
-function FriendsList(){}
-function Friend(){}
-function AddFriendForm(){}
-function SplitBillForm(){}
+function FriendsList() {
+  return (
+    <div>
+      {initialFriends.map((friend) => (
+        <Friend
+          name={friend.name}
+          image={friend.image}
+          balance={friend.balance}
+          key={friend.id}
+        />
+      ))}
+    </div>
+  );
+}
+function Friend({ name, image, balance }) {
+  return (
+    <div>
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+      <button>Select</button>
+      <p>{balance}</p>
+    </div>
+  );
+}
+function AddFriendForm() {}
+function SplitBillForm() {}
